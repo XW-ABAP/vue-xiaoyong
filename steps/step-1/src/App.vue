37867cc15ce8c1dd61b9b6fe0696306edda91b3b
XW-abap <yongxuanyisheng@gmail.com>
2026-05-26 23:36:03 +0800

    add
    add

--- steps/step-1/src/App.vue
+++ steps/step-1/src/App.vue
@@ -2,6 +2,10 @@
   <h3>模板语法</h3>
   <p>{{ msg }}</p>
   <p>{{ hello }}</p>
+  <p>{{ number + 1 }}</p>
+  <p>{{ ok ? 'ok' : 'not ok' }}</p>
+  <p>{{ message.split('').reverse().join('') }}</p>
+  <div v-html="rawHtml"></div>
 </template>
 
 
@@ -10,7 +14,11 @@ export default {
   data(){
     return{
       msg: 'Hello Vue!',
-      hello: 'Hello World!'
+      hello: 'Hello World!',
+      number: 123,
+      ok: true,
+      message: 'Hello Vue.js!',
+      rawHtml: "<a href='https://www.baidu.com'>百度一下</a>"
     }
   }
 }
