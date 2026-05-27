6b8ab778a92bfb859299ef048269a06ce2042992
XW-abap <yongxuanyisheng@gmail.com>
2026-05-28 00:07:55 +0800

    add
    add

---
+++ steps/step-3/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import IFDemo from './components/IFDemo.vue'
+</script>
+
+<template>
+  <HelloWorld/>
+  <IFDemo />
+</template>
+
---
+++ steps/step-3/src/components/IFDemo.vue
@@ -0,0 +1,22 @@
+<template>
+    <h3>条件渲染</h3>
+    <div v-if="flag">flag is true</div>
+    <div v-else>flag is false</div>
+    <div v-if="type === 'A'">type is A</div>
+    <div v-else-if="type === 'B'">type is B</div>
+    <div v-else-if="type === 'C'">type is C</div>
+    <div v-else>type is other</div>
+    <div v-show="flag">flag is {{ flag }}</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            // flag: true,
+            flag: true,
+            type: 'D'
+        }
+    }
+}
+</script>
